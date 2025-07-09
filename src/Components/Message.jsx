6b22fac2c2e7c975b38d86adc6a3cea2c862818a
agrannya <singh.agrannya@gmail.com>
2025-07-10 00:33:42 +0530

    Update Message.jsx

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,7 +1,7 @@
 const Message = ({ message }) => {
     return (
         <div id={message.id} className={`message ${message.role}-message ${message.loading ? "loading" : ""} ${message.error ? "error" : ""}`}>
-            {message.role === "bot" && <img className="avatar" src="src/assets/Google_Gemini_logo.png" alt="Bot Avatar" />}
+            {message.role === "bot" && <img className="avatar" src="https://upload.wikimedia.org/wikipedia/commons/8/8a/Google_Gemini_logo.svg" alt="Bot Avatar" />}
             <p className="text">{message.content}</p>
         </div>
     );
